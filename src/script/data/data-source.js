643074ffb84764c3c6e5dd32d32c477969fc3787
atha <athabarokahfc@gmail.com>
2023-08-14 16:21:01 +0000

    menambahkan async/await dan try/catch

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -1,12 +1,21 @@
 // Menjalankan fungsi mengambil data dari source club
-  function searchClub(keyword) {
+class DataSource {
+  // Method yang diakses langsung tanpa pembuatan instance
+ static searchClub(keyword) {
     return new Promise( (resolve,reject) => {
-     const filteredClubs = clubs.filter( (club) => {return club.name.toUpperCase().includes(keyword.toUpperCase()) } ) ;
-     if (filteredClubs.length) {
-      resolve(filteredClubs);
-      console.log(filteredClubs)
-  } else {
-    reject(keyword + ' is not found');
-  }
+
+      setTimeout( () => {
+        const filteredClubs = clubs.filter( (club) => {return club.name.toUpperCase().includes(keyword.toUpperCase()) } ) ;
+       if (filteredClubs.length) {
+        resolve(filteredClubs);
+        console.log(filteredClubs)
+        } else {
+        reject(keyword + ' is not found');
+        }
+        
+      } ,2000);
+     
     });
   }
+  
+}
--- src/script/view/main.js
+++ src/script/view/main.js
@@ -3,38 +3,49 @@ const main = () => {
   const searchVal = document.querySelector('#searchElement');
   const buttonSearchElement = document.querySelector('#searchButtonElement');
   const clubListElement = document.querySelector('#clubList');
-  
+
   // Event Ketika diklik 
-  buttonSearchElement.addEventListener('click', function () {
-    let keyword = searchVal.value ; 
-    searchClub(keyword)
-      .then(resolve => {
-        renderClub(resolve) ;
-        console.log(resolve) ; 
-      }) 
-      .catch(reject => renderFail(reject) ) ; 
+  buttonSearchElement.addEventListener('click', () => {
+    onButtonClicked();
+    console.log('Sedang mengambil data');
   });
 
-  
+  // buttonSearchElement.addEventListener('click', () => console.log('Sedang mengambil data'));
+
+  // Kumpulan fungsi yang saling berkaitan dan menunggu 
+  async function onButtonClicked() {
+
+    try {
+      let keyword = searchVal.value;
+      let dataClub = await DataSource.searchClub(keyword);
+      renderClub(dataClub);
+
+
+    } catch (pesan) {
+      renderFail(pesan);
+    }
+  }
+
+
 
   // Merender perubahan jika sukses 
   function renderClub(clubs) {
     // Clearing inner HTML untuk refresh
-    clubListElement.innerHTML = '' ;
+    clubListElement.innerHTML = '';
     clubs.forEach(club => {
       let clubEl = document.createElement('div');
-      clubEl.classList.add('club') ;
+      clubEl.classList.add('club');
 
-      const {name, fanArt, description}  = club ; 
-      
-      clubEl.innerHTML =  `
+      const { name, fanArt, description } = club;
+
+      clubEl.innerHTML = `
         <img class="fan-art-club" src="${fanArt}" alt="Fan Art" /> 
         <div class="club-info" >
         <h2> ${name} </h2>
         <p> ${description} </P> 
         </div>
-        ` ;  
-      clubListElement.append(clubEl) ; 
+        ` ;
+      clubListElement.append(clubEl);
     });
   }
 
@@ -43,9 +54,9 @@ const main = () => {
     clubListElement.innerHTML = '';
     clubListElement.innerHTML += '<h2 class="placeholder">' + reject + '</h2>';
   }
-  
-  
-  
-} ; 
+
+
+
+};
 
 
